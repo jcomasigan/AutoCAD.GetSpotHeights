d0790db9c90c80d06223562b975c496f5c2af0ff
Jericho Masigan <jco.masigan@gmail.com>
2016-06-06 18:21:14 +1200

    1

---
+++ GetSpotHeightForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace GetSpotHeights
+{
+    partial class GetSpotHeightForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.blockDwgBtn = new System.Windows.Forms.Button();
+            this.checkEveryVertext = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.blockScaleTxtBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.plotButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.plineLengthLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Block to insert:";
+            // 
+            // blockDwgBtn
+            // 
+            this.blockDwgBtn.Location = new System.Drawing.Point(122, 22);
+            this.blockDwgBtn.Name = "blockDwgBtn";
+            this.blockDwgBtn.Size = new System.Drawing.Size(132, 23);
+            this.blockDwgBtn.TabIndex = 1;
+            this.blockDwgBtn.Text = "......";
+            this.blockDwgBtn.UseVisualStyleBackColor = true;
+            this.blockDwgBtn.Click += new System.EventHandler(this.blockDwgBtn_Click);
+            // 
+            // checkEveryVertext
+            // 
+            this.checkEveryVertext.AutoSize = true;
+            this.checkEveryVertext.Location = new System.Drawing.Point(19, 69);
+            this.checkEveryVertext.Name = "checkEveryVertext";
+            this.checkEveryVertext.Size = new System.Drawing.Size(159, 17);
+            this.checkEveryVertext.TabIndex = 2;
+            this.checkEveryVertext.Text = "Or put height in every vertex";
+            this.checkEveryVertext.UseVisualStyleBackColor = true;
+            this.checkEveryVertext.CheckedChanged += new System.EventHandler(this.checkEveryVertext_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Offset(m):";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.blockScaleTxtBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.blockDwgBtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 103);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Block Details";
+            // 
+            // blockScaleTxtBox
+            // 
+            this.blockScaleTxtBox.Location = new System.Drawing.Point(122, 63);
+            this.blockScaleTxtBox.Name = "blockScaleTxtBox";
+            this.blockScaleTxtBox.Size = new System.Drawing.Size(37, 20);
+            this.blockScaleTxtBox.TabIndex = 3;
+            this.blockScaleTxtBox.Text = "1.0";
+            this.blockScaleTxtBox.Leave += new System.EventHandler(this.blockScaleTxtBox_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Block Scale:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.plineLengthLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.checkEveryVertext);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 106);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Labelling Options";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "6.0";
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // plotButton
+            // 
+            this.plotButton.Enabled = false;
+            this.plotButton.Location = new System.Drawing.Point(61, 236);
+            this.plotButton.Name = "plotButton";
+            this.plotButton.Size = new System.Drawing.Size(198, 23);
+            this.plotButton.TabIndex = 1;
+            this.plotButton.Text = "Plot Heights";
+            this.plotButton.UseVisualStyleBackColor = true;
+            this.plotButton.Click += new System.EventHandler(this.plotButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Selected Pline Length:";
+            // 
+            // plineLengthLabel
+            // 
+            this.plineLengthLabel.AutoSize = true;
+            this.plineLengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plineLengthLabel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.plineLengthLabel.Location = new System.Drawing.Point(136, 42);
+            this.plineLengthLabel.Name = "plineLengthLabel";
+            this.plineLengthLabel.Size = new System.Drawing.Size(13, 13);
+            this.plineLengthLabel.TabIndex = 0;
+            this.plineLengthLabel.Text = "0";
+            // 
+            // GetSpotHeightForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 273);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.plotButton);
+            this.Name = "GetSpotHeightForm";
+            this.Text = "Get Spot Heights";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button blockDwgBtn;
+        private System.Windows.Forms.CheckBox checkEveryVertext;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox blockScaleTxtBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button plotButton;
+        private System.Windows.Forms.Label plineLengthLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ GetSpotHeightForm.cs
@@ -0,0 +1,255 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetSpotHeights
+{
+    public partial class GetSpotHeightForm : Form
+    {
+        string attributeTagName = "";
+        string blockName = "";
+        double blockScale = 1;
+        double intervalPline = 0.5; 
+        PromptEntityResult prmtEnt;
+
+        public GetSpotHeightForm(PromptEntityResult prmtEntIncoming)
+        {
+            InitializeComponent();
+            prmtEnt = prmtEntIncoming;
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                DBObject obj = tr.GetObject(prmtEnt.ObjectId, OpenMode.ForRead);
+                Polyline3d polyline = obj as Polyline3d;
+                decimal lngth = Math.Round(Convert.ToDecimal(polyline.Length), 2);
+                plineLengthLabel.Text = lngth.ToString();
+            }
+        }
+
+        private void plotButton_Click(object sender, EventArgs e)
+        {
+            int blockCount = 0;
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+            intervalPline = Convert.ToDouble(textBox2.Text);
+            Database blockDB = new Database(true, true);
+            ObjectId insertedBlockId;
+            try
+            {
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    double lengthToNextVertex = 0;
+                    DBObject obj = tr.GetObject(prmtEnt.ObjectId, OpenMode.ForRead);
+                    Polyline3d polyline = obj as Polyline3d;
+                    if (polyline != null)
+                    {
+                        List<ObjectId> vertices = new List<ObjectId>();
+                        foreach(ObjectId id in polyline)
+                        {
+                            vertices.Add(id);
+                        }
+                        for (int i = 0; i < vertices.Count - 1; i++)
+                        {
+                            
+                            PolylineVertex3d vertex = tr.GetObject(vertices[i], OpenMode.ForRead) as PolylineVertex3d;
+                            PolylineVertex3d nextVertex = tr.GetObject(vertices[i + 1], OpenMode.ForRead) as PolylineVertex3d;
+                            BlockTable blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                            Double length = vertex.Position.DistanceTo(nextVertex.Position);
+                            lengthToNextVertex += length;
+                            if (checkEveryVertext.CheckState != CheckState.Checked)
+                            {
+                                while (lengthToNextVertex >= intervalPline)
+                                {
+                                    Point3d pt = GetPt(vertex.Position, nextVertex.Position, intervalPline - (lengthToNextVertex - length));
+                                    lengthToNextVertex -= intervalPline;
+                                    if (blockTable.Has(blockName))
+                                    {
+                                        BlockTableRecord blockTableRecord = blockTable[blockName].GetObject(OpenMode.ForRead) as BlockTableRecord;
+                                        if (blockTable != null)
+                                        {
+                                                using (BlockReference spotHeight = new BlockReference(pt, blockTableRecord.ObjectId))
+                                                {
+                                                    blockCount++;
+                                                    using (DocumentLock doclock = doc.LockDocument())
+                                                    {
+                                                        spotHeight.ScaleFactors = new Autodesk.AutoCAD.Geometry.Scale3d(blockScale, blockScale, blockScale);
+                                                        BlockTableRecord blocktableRec = tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                                                        blocktableRec.AppendEntity(spotHeight);
+                                                        tr.AddNewlyCreatedDBObject(spotHeight, true);
+                                                        insertedBlockId = spotHeight.ObjectId;
+                                                        foreach (ObjectId attrID in blockTableRecord)
+                                                        {
+                                                            DBObject atttObj = attrID.GetObject(OpenMode.ForRead);
+                                                            AttributeDefinition attrib = atttObj as AttributeDefinition;
+                                                            if ((attrib != null) && (!attrib.Constant))
+                                                            {
+                                                                using (AttributeReference attributeRef = new AttributeReference())
+                                                                {
+                                                                    attributeRef.SetAttributeFromBlock(attrib, spotHeight.BlockTransform);
+                                                                    attributeRef.TextString = Decimal.Round(Convert.ToDecimal(spotHeight.Position.Z), 2, MidpointRounding.AwayFromZero).ToString();
+
+                                                                    spotHeight.AttributeCollection.AppendAttribute(attributeRef);
+                                                                    tr.AddNewlyCreatedDBObject(attributeRef, true);
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (blockTable.Has(blockName))
+                                {
+                                    BlockTableRecord blockTableRecord = blockTable[blockName].GetObject(OpenMode.ForRead) as BlockTableRecord;
+                                    if (blockTable != null)
+                                    {
+                                            using (BlockReference spotHeight = new BlockReference(vertex.Position, blockTableRecord.ObjectId))
+                                            {
+                                                blockCount++;
+                                                using (DocumentLock doclock = doc.LockDocument())
+                                                {
+                                                    spotHeight.ScaleFactors = new Autodesk.AutoCAD.Geometry.Scale3d(blockScale, blockScale, blockScale);
+                                                    BlockTableRecord blocktableRec = tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                                                    blocktableRec.AppendEntity(spotHeight);
+                                                    tr.AddNewlyCreatedDBObject(spotHeight, true);
+                                                    insertedBlockId = spotHeight.ObjectId;
+                                                    foreach (ObjectId attrID in blockTableRecord)
+                                                    {
+                                                        DBObject atttObj = attrID.GetObject(OpenMode.ForRead);
+                                                        AttributeDefinition attrib = atttObj as AttributeDefinition;
+                                                        if ((attrib != null) && (!attrib.Constant))
+                                                        {
+                                                            using (AttributeReference attributeRef = new AttributeReference())
+                                                            {
+                                                                attributeRef.SetAttributeFromBlock(attrib, spotHeight.BlockTransform);
+                                                                attributeRef.TextString = Decimal.Round(Convert.ToDecimal(spotHeight.Position.Z), 2, MidpointRounding.AwayFromZero).ToString();
+
+                                                                spotHeight.AttributeCollection.AppendAttribute(attributeRef);
+                                                                tr.AddNewlyCreatedDBObject(attributeRef, true);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        
+                                    }
+                                }
+                            }
+                        }
+
+                        tr.Commit();
+                    }
+                    ed.Regen();
+                    this.Close();
+                    ed.WriteMessage("Successfully added {0} instances of block {1}", blockCount, blockName);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private Point3d GetPt(Point3d ptStart, Point3d ptEnd, double divisor)
+        {
+            Line3d line = new Line3d(ptStart, ptEnd);
+            Point3d pt = line.EvaluatePoint(divisor);
+            return pt;
+        }
+
+        private double GetLength(Point3d ptStart, Point3d ptEnd)
+        {
+            LineSegment3d line = new LineSegment3d(ptStart, ptEnd);
+            return line.Length;
+        }
+        private void blockDwgBtn_Click(object sender, EventArgs e)
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            Database db = doc.Database;
+            this.Hide();
+            PromptEntityOptions prmtOpt = new PromptEntityOptions("\n\nSelect block");
+
+            prmtOpt.SetRejectMessage("\n\nCan only select a block definition");
+            prmtOpt.AddAllowedClass(typeof(BlockReference), false);
+            PromptEntityResult result = ed.GetEntity(prmtOpt);
+            if(result.Status == PromptStatus.OK)
+            {
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    BlockReference blkReference = tr.GetObject(result.ObjectId, OpenMode.ForRead) as BlockReference;
+                    BlockTableRecord blockRecord = null;
+                    if(blkReference.IsDynamicBlock)
+                    {
+                        blockRecord = tr.GetObject(blkReference.DynamicBlockTableRecord, OpenMode.ForRead) as BlockTableRecord;                        
+                    }
+                    else
+                    {
+                        blockRecord = tr.GetObject(blkReference.BlockTableRecord, OpenMode.ForRead) as BlockTableRecord;
+                    }
+                    blockName = blockRecord.Name;
+                    blockDwgBtn.Text = blockRecord.Name;
+                    this.Show();
+                    this.Focus();
+                    plotButton.Enabled = true;
+                }
+            }
+        }
+
+        private void blockScaleTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blockScaleTxtBox_Leave(object sender, EventArgs e)
+        {
+            if(double.TryParse(blockScaleTxtBox.Text, out blockScale))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (double.TryParse(blockScaleTxtBox.Text, out intervalPline))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+            }
+        }
+
+        private void checkEveryVertext_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkEveryVertext.CheckState == CheckState.Checked)
+            {
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
+            }
+        }
+    }
+}
